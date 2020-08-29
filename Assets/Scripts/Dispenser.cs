73660f974360efa863a7da2e40e90da2cbd7215a
Mangonels <dylancalaflatham@gmail.com>
2020-08-29 18:32:45 +0200

    FIRST!

---
+++ Assets/Scripts/Dispenser.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dispenser : MonoBehaviour
+{
+    public Object objectToSpawn;
+
+    void Start()
+    {
+        Instantiate(objectToSpawn, transform.position, Quaternion.Euler(0, 0, 0));
+    }
+
+    void Update()
+    {
+        
+    }
+}
